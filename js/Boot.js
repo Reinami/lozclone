11eb66861d531638631079d3257281cf9ade6daf
unknown <Typhon@.(none)>
2015-07-31 23:49:18 -0700

    .

--- js/Boot.js
+++ js/Boot.js
@@ -11,19 +11,6 @@ Unicorns.Boot.prototype = {
 		// Loading Screen background
 		this.game.stage.backgroundColor = '#fff';
 
-		// Set scaling options
-		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		this.scale.minWidth = 240;
-		this.scale.minHeight = 170;
-		this.scale.maxWidth = 2880;
-		this.scale.maxHeight = 1920;
-
-		// Center the Game
-		this.scale.pageAlignHorizontally = true;
-
-		// Automatically set screen size
-		this.scale.setScreenSize(true);
-
 		// Physics
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
